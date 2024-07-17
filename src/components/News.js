4e097ed45fb0a11a0c75339912b90ad5a719000d
sansidsac <kssanju04@gmail.com>
2024-07-17 18:24:10 +0530

    Added Navigation between news pages

--- src/components/News.js
+++ src/components/News.js
@@ -11,19 +11,39 @@ export class News extends Component {
             page:1
         }
     }
-    handleNextClick=()=>{
-        this.setState({page:2});
-    }
-    handlePrevClick=()=>{
-        this.setState({page:1});
-    }
 
     async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=429348170a234edf8d313236f6579cfd&page=${this.page}`;
+      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=429348170a234edf8d313236f6579cfd&page=1&pageSize=20";
+      let data=await fetch(url);
+      let parsedData=await data.json();
+      this.setState({articles:parsedData.articles, totalArticles:parsedData.totalResults});
+  }
+
+    handleNextClick= async ()=>{
+      if(this.state.page + 1 > Math.ceil(this.state.totalArticles/20)){
+
+      }
+      else{
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=429348170a234edf8d313236f6579cfd&page=${this.state.page+1}&pageSize=20`;
         let data=await fetch(url);
         let parsedData=await data.json();
-        this.setState({articles:parsedData.articles});
+        this.setState({
+          page: this.state.page+1,
+          articles:parsedData.articles
+        });
+      }
     }
+    handlePrevClick= async ()=>{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=429348170a234edf8d313236f6579cfd&page=${this.state.page-1}&pageSize=20`;
+      let data=await fetch(url);
+      let parsedData=await data.json();
+      this.setState({
+        page: this.state.page-1,
+        articles:parsedData.articles
+      });
+    }
+
+
     
   render() {
     return (
@@ -39,7 +59,7 @@ export class News extends Component {
         <div className="container my-4">
         <div className="d-flex mb-3">
   <div className="p-2">
-  <button type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+  <button  disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
   </div>
   <div className="ms-auto p-2">
     <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
