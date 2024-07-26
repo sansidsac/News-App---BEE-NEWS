3dc2fba146ae4558489d20df3ab38d9b733f7468
sansidsac <kssanju04@gmail.com>
2024-07-26 14:53:49 +0530

    Added infinite scroll and removed next/prev buttons

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ export default class App extends Component {
         <div>
           <NavBar />
           <Routes>
-            <Route path="/" element={<News key="general" pageSize={15} country={'in'} category={'general'} />} />
-            <Route path="/science" element={<News key="science" pageSize={15} country={'in'} category={'science'} />} />
-            <Route path="/technology" element={<News key="technology" pageSize={15} country={'in'} category={'technology'} />} />
-            <Route path="/business" element={<News key="business" pageSize={15} country={'in'} category={'business'} />} />
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={15} country={'in'} category={'entertainment'} />} />
-            <Route path="/health" element={<News key="health" pageSize={15} country={'in'} category={'health'} />} />
-            <Route path="/sports" element={<News key="sports" pageSize={15} country={'in'} category={'sports'} />} />
+            <Route path="/" element={<News key="general" pageSize={6} country={'in'} category={'general'} />} />
+            <Route path="/science" element={<News key="science" pageSize={6} country={'in'} category={'science'} />} />
+            <Route path="/technology" element={<News key="technology" pageSize={6} country={'in'} category={'technology'} />} />
+            <Route path="/business" element={<News key="business" pageSize={6} country={'in'} category={'business'} />} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={6} country={'in'} category={'entertainment'} />} />
+            <Route path="/health" element={<News key="health" pageSize={6} country={'in'} category={'health'} />} />
+            <Route path="/sports" element={<News key="sports" pageSize={6} country={'in'} category={'sports'} />} />
           </Routes>
         </div>
       </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Loading from './Loading';
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
     static defaultProps={
@@ -19,7 +20,8 @@ export class News extends Component {
         this.state={
             articles: [],
             loading: true,
-            page:1
+            page:1,
+            totalResults:0
         }
         document.title=`BEE NEWS ${this.capitalizeFirstLetter(this.props.category)}`;
     }
@@ -39,15 +41,25 @@ export class News extends Component {
       this.updateNews();
   }
 
+  fetchMoreData = async () => {
+    this.setState({page:this.state.page +1});
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=429348170a234edf8d313236f6579cfd&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalArticles:parsedData.totalResults
+    });
+  };
 
-  handleNextClick= async ()=>{
-      this.setState({page:this.state.page+1});
-      this.updateNews();
-    }
-    handlePrevClick= async ()=>{
-      this.setState({page:this.state.page-1});
-      this.updateNews();
-    }
+  // handleNextClick= async ()=>{
+  //     this.setState({page:this.state.page+1});
+  //     this.updateNews();
+  //   }
+  //   handlePrevClick= async ()=>{
+  //     this.setState({page:this.state.page-1});
+  //     this.updateNews();
+  //   }
 
     capitalizeFirstLetter=(string)=>{
       return string.charAt(0).toUpperCase()+string.slice(1);
@@ -55,17 +67,28 @@ export class News extends Component {
     
   render() {
     return (
-      <div className='container my-4'>
+      <>
+        <div className="container my-4">
         <h1 className='text-center'>BEE NEWS - Top Headlines from {this.capitalizeFirstLetter(this.props.category)} Category</h1>
+        </div>
         {this.state.loading && <Loading/>}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={!(this.state.page + 1 > Math.ceil(this.state.totalArticles/this.props.pageSize))}
+          loader={!this.state.loading && <Loading/>}
+        >
+        <div className="container my-2">
         <div className='row my-3'>
-        {!this.state.loading && this.state.articles.map((element)=>{
+        {this.state.articles.map((element)=>{
             return <div className='col-md-4'  key={element.url}>
             <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
             </div>
         })}     
         </div>
-        <div className="container my-4">
+        </div>
+        </InfiniteScroll>
+        {/* <div className="container my-4">
         <div className="d-flex mb-3">
   <div className="p-2">
   <button  disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
@@ -74,10 +97,8 @@ export class News extends Component {
     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
     </div>
 </div>
-        
-        
-        </div>
-      </div>
+        </div> */}
+      </>
     )
   }
 }
